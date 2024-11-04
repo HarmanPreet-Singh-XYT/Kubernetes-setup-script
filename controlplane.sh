2c265dcc361580ef5c9fc06f649aa5f390b0625e
HarmanPreet-Singh-XYT <preetsinghharman27@gmail.com>
2024-11-04 19:15:17 +0530

    network plugin error

--- controlplane.sh
+++ controlplane.sh
@@ -68,7 +68,7 @@ kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.27.0
 wget https://raw.githubusercontent.com/projectcalico/calico/v3.27.0/manifests/custom-resources.yaml
 sed -i 's/192\.168\.0\.0\/16/10.244.0.0\/16/g' custom-resources.yaml
 kubectl apply -f custom-resources.yaml
-
+kubectl apply -f https://raw.githubusercontent.com/projectcalico/calico/v3.27.0/manifests/tigera-operator.yaml
 
 # Generate join command for worker nodes
 JOIN_CMD=$(kubeadm token create --print-join-command)
